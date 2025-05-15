bff8b0adffb04d5d226a54462c846f7ce3fd1c2a
yanxiao-yx <15212532887@163.com>
2025-05-15 10:21:25 +0800

    Add files via upload

---
+++ my-vue3-project/src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+const count = ref(0)
+const isDisabled = computed(() => count.value >= 5)
+
+const increment = () => {
+  if (!isDisabled.value) {
+    count.value++
+  }
+}
+</script>
+
+<template>
+  <div class="card">
+    <button
+      type="button"
+      @click="increment"
+      :disabled="isDisabled"
+      id="increment-btn"
+    >
+      count is {{ count }}
+    </button>
+    <p v-if="isDisabled" class="warning">已达到最大点击次数</p>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ my-vue3-project/src/main.ts
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+// import ElementPlus from 'element-plus'
+// import zhCn from 'element-plus/es/locale/lang/zh-cn'
+
+// App.use(ElementPlus, {
+//   locale: zhCn,
+// })
+createApp(App).mount('#app')
